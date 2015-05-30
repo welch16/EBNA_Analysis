d3ff3d5831b0835c25242f59b2c3c670fdd7b19e
welch16 <welch@stat.wisc.edu>
2015-05-29 19:09:08 -0500

    Added heatmaps and barplots to md document

--- R/heatmap_functions.R
+++ R/heatmap_functions.R
@@ -3,7 +3,7 @@ build_binary_matrix <-
 function(peakSet,ranges,expr = NULL,col_expr = NULL,rem.Col.zero = TRUE  )
 {
    # Returns a binary matrix built from the ranges objects
-   columns = get_names(ranges[[peakSet]])
+   columns = get_names(ranges)
    columns = subset(columns,eval(parse(text = grepl_cmd("summit","columns"))))
    columns = subset(columns,eval(parse(text = grepl_cmd("region","columns"))))
    columns = subset(columns,eval(parse(text = grepl_cmd("allTF","columns"))))
@@ -15,7 +15,7 @@ function(peakSet,ranges,expr = NULL,col_expr = NULL,rem.Col.zero = TRUE  )
      columns = subset(columns,eval(parse(text = col_expr)))
    }
    if(!is.null(expr)){
-     sub = subset(ranges[[peakSet]],select = columns,
+     sub = subset(ranges,select = columns,
        subset = eval(parse(text = expr))) 	
    }else{
      sub = subset(ranges[[peakSet]],select = columns)
@@ -32,4 +32,84 @@ get_proportions <-
 function(mat)
 {
   return(sort(colSums(mat) / dim(mat)[1],decreasing = TRUE))
+}
+
+gg_heatmap <- function(mat,title = NULL)
+{
+
+  hh = hclust(dist(mat))
+  vv = hclust(dist(t(mat)))
+
+  hh1 = as.dendrogram(hh)
+  vv1 = as.dendrogram(vv)
+  
+  h_order = order.dendrogram(reorder(hh1,rowMeans(mat)))
+  v_order = order.dendrogram(reorder(vv1,colMeans(mat)))
+
+  row_names = rownames(mat)
+  col_names = colnames(mat)
+
+  df = melt(mat)
+  names(df) = c("Var1","Var2","value")
+  
+  df$Var1 = factor(df$Var1,levels = row_names[h_order])
+  df$Var2 = factor(df$Var2,levels = rev(col_names[v_order]))
+
+  p1 <- ggplot(df,aes(Var2,Var1))+geom_tile(aes(fill= factor(value)))+
+    scale_y_discrete(expand = c(0,0))+scale_x_discrete(expand = c(0,0))+
+      labs(x="",y="")+theme(legend.position = "bottom",
+      axis.ticks = element_blank(),axis.text.y = element_blank())+
+      theme(axis.text.x = element_text(angle = -90,size= 6.2,hjust= 0),
+            plot.margin = unit(c(0,1,.5,1),"cm"),
+            axis.ticks.y = element_blank())+
+      scale_fill_manual(name = "Overlap",values = c("lightblue","navyblue"),labels = c("no","yes"))+
+      scale_x_discrete(expand = c(0, 0)) + scale_y_discrete(expand = c(0, 0))
+  
+
+  ddata_y <- dendro_data(vv1)
+
+  ### Set up a blank theme
+  theme_none <- theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      axis.title.x = element_text(colour=NA),
+      axis.title.y = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.line = element_blank()     
+      )
+  
+
+  # Dendrogram 2
+  p3 <- ggplot(segment(ddata_y)) +
+      geom_segment(aes(x=x, y=y, xend=xend, yend=yend)) +
+      theme_none+labs(x=NULL,y=NULL)+
+      theme(plot.margin = unit(c(.5,0,0,0),"cm"))+
+      scale_x_continuous(expand = c(0, 0)) +
+        scale_y_continuous(expand = c(0, 0))+
+        theme(axis.ticks.y = element_blank())
+
+  if(!is.null(title)){
+    p3 <- p3 + ggtitle(title)
+  }
+  
+  return(list(p1,p3))  
+  
+}
+
+print_hm <- function(x)
+{
+  p1 <- x[[1]]
+  p2 <- x[[2]]
+
+  g <- ggplotGrob(p1)
+
+  g <- gtable_add_rows(g, unit(1.2,"in"), 0)
+  g <- gtable_add_grob(g, ggplotGrob(p2) ,
+         t = 1, l=4, b=1, r=4)
+
+  grid.newpage()
+  grid.draw(g)
+ 
 }
--- notes/01_heatmap_analysis.md
+++ notes/01_heatmap_analysis.md
@@ -7,6 +7,33 @@ are already grouped by transcription factors.
 
 
 
+#### Filtering to peaks that overlap Dnase hypersensitive sites
+
+We generate the matrices and proportion vectors:
+
+
+```r
+mats <- mcmapply(build_binary_matrix, names(ranges),ranges,
+  MoreArgs = list(expr = "Dnase == 1",
+             col_expr = cols_to_remove(pattern,'columns')),
+			 SIMPLIFY=FALSE,mc.silent=TRUE,mc.cores = mc)
+
+proportions <- lapply(mats,get_proportions)
+```
+
+
+
+![plot of chunk hm_dnaseFilter ](../figures/heatmaps/hm_dnaseFilter -1.png) ![plot of chunk hm_dnaseFilter ](../figures/heatmaps/hm_dnaseFilter -2.png) ![plot of chunk hm_dnaseFilter ](../figures/heatmaps/hm_dnaseFilter -3.png) ![plot of chunk hm_dnaseFilter ](../figures/heatmaps/hm_dnaseFilter -4.png) ![plot of chunk hm_dnaseFilter ](../figures/heatmaps/hm_dnaseFilter -5.png) 
+
+If we calculate the proportions of each TF, and sort them we can get the following:
+
+![plot of chunk proportions](../figures/heatmaps/proportions-1.png) ![plot of chunk proportions](../figures/heatmaps/proportions-2.png) ![plot of chunk proportions](../figures/heatmaps/proportions-3.png) ![plot of chunk proportions](../figures/heatmaps/proportions-4.png) ![plot of chunk proportions](../figures/heatmaps/proportions-5.png) 
+
+
+
+
+
+
 
 
 
