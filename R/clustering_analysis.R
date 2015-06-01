c4abd0344b81ddfe92b812c8485d5d1af82afdfd
welch16 <welch@stat.wisc.edu>
2015-06-01 12:21:30 -0500

    Added function to find the TFs that appear by top and cutline strategies

--- R/clustering_analysis.R
+++ R/clustering_analysis.R
@@ -7,3 +7,31 @@ pam_silhouette_dt <- function(mat,k,...)
     neighbor =sil[,2],width=sil[,3])
   return(sil_dt)
 }
+
+
+
+top_k <- function(V1,variable,k,decreasing = TRUE)
+{
+  names(V1) = as.character(variable)
+  u = sort(V1,decreasing = decreasing)
+  u = u[1:k]
+  nm = names(u)
+  names(u) = NULL
+  return(list(u,nm))
+}
+
+  
+
+top_strategy <- function(peak,props,top)
+{
+  top_TF <- props[set == peak, top_k(V1,variable,top),by=.(set,clustering)]
+  TFs <- unique(top_TF[,(V2)])
+  return(TFs)
+}
+
+cut_strategy <- function(peak,props,cutline)
+{
+  cut_TF <- props[set == peak & V1 >= cutline]
+  TFs <- as.character(unique(cut_TF[,(variable)]))
+  return(TFs)
+}
