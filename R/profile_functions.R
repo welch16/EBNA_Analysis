1881f02b23d8ffffa32da2571c3518780f0f982f
welch16 <welch@stat.wisc.edu>
2015-09-06 21:39:34 -0500

    Draft of heatmap analysis and functions to perform it

---
+++ R/profile_functions.R
@@ -0,0 +1,49 @@
+
+load_clean_profile <- function(file ,window_ext)
+{
+  load(file) ## profile
+  profile[,match := paste(chr,match,sep = "_")]
+  profile[,chr := NULL]
+  profile <- profile[order(match,coord)]
+  npeaks <- nrow(profile) / (2 * window_ext + 1)
+  profile[,coord := rep(-window_ext:window_ext, npeaks)]
+  return(profile)
+}
+
+
+grepV <- function(pattern,x,ignore.case = FALSE,perl = FALSE,value = FALSE,
+  fixed = FALSE,useBytes = FALSE, invert = FALSE)
+{
+  return(x[grep(pattern,x,ignore.case = FALSE,perl = FALSE,value = FALSE,
+  fixed = FALSE,useBytes = FALSE, invert = FALSE)])
+}
+
+add_names <- function(set,data,mc = 24)
+{
+    out <- mcmapply(function(x,y){   
+      v <- strsplit(y,"_")[[1]]
+      aux <- copy(x)
+      aux[,set := v[1]]
+      z <- strsplit(v[2],"-")[[1]]
+      aux[,histone := z[1]]
+      aux[,rep := as.numeric(z[2])]
+      return(aux)
+    },data[[set]],names(data[[set]]),SIMPLIFY = FALSE,mc.cores = mc)
+  out <- do.call(rbind,out)
+  return(out)
+}
+
+add_match <- function(set,unified_lists,factor_overlaps)
+{
+  peaks <- cbind(unified_lists$peaks,unified_lists$overlaps,factor_overlaps)
+  chr <- unique(peaks[,(seqnames)])  
+  setkey(peaks,seqnames)
+  idx <- which(peaks[[set]] == 1) 
+  lengths <- peaks[idx][,length(start),by = seqnames]
+  peaks <- peaks[idx]
+  peaks[,match := ""]
+  setkey(lengths,seqnames)
+  matchs <- lengths[,paste(seqnames,1:V1,sep = "_"),by = seqnames][,(V1)]
+  peaks[order(seqnames,start),match := matchs]
+  return(peaks)
+}
--- scripts/histone_signal_analysis.R
+++ scripts/histone_signal_analysis.R
@@ -9,7 +9,9 @@ library(RColorBrewer)
 library(parallel)
 library(pracma)
 
-profile_dir <- "data/RData/profile_matrices"
+source("R/profile_functions.R")
+
+profile_dir <- "data/RData/profile_matrices_old"
 
 sets <- c("EBNA2","EBNA3A","EBNA3B","EBNA3C","RBPJ")
 
@@ -20,96 +22,78 @@ mc <- detectCores()
 load("data/RData/unified_lists_wProbs.RData")
 load("data/RData/factor_overlaps.RData")
 
-load_clean_profile <- function(file ,window_ext)
-{
-  load(file) ## profile
-  profile[,match := paste(chr,match,sep = "_")]
-  profile[,chr := NULL]
-  profile <- profile[order(match,coord)]
-  npeaks <- nrow(profile) / (2 * window_ext + 1)
-  profile[,coord := rep(-window_ext:window_ext, npeaks)]
-  return(profile)
-}
+profiles <- lapply(sets,
+  function(x,dir){
+    files <- grepV(x,files)
+    out <- mclapply(file.path(dir,files),
+      load_clean_profile,window_ext,mc.cores = mc)
+    names(out) <- files
+    return(out)},profile_dir)
+names(profiles) <- sets
+
+peak_list <- lapply(sets,add_match,unified_lists,factor_overlaps)
+names(peak_list) <- sets
 
 
-grepV <- function(pattern,x,ignore.case = FALSE,perl = FALSE,value = FALSE,
-  fixed = FALSE,useBytes = FALSE, invert = FALSE)
-{
-  return(x[grep(pattern,x,ignore.case = FALSE,perl = FALSE,value = FALSE,
-  fixed = FALSE,useBytes = FALSE, invert = FALSE)])
-}
-
-
-files1 <- grepV("EBNA3A",files)
-files2 <- grepV("EBNA3B",files)
-files3 <- grepV("EBNA3C",files)
-files4 <- grepV("EBNA2",files)
-files5 <- grepV("RBPJ",files)
-
-profiles <- list()
-profiles[["EBNA3A"]] <- mclapply(file.path(profile_dir,files1),
-  load_clean_profile,window_ext, mc.cores = mc,mc.preschedule = TRUE)
-
-profiles[["EBNA3B"]] <- mclapply(file.path(profile_dir,files2),
-  load_clean_profile,window_ext, mc.cores = mc,mc.preschedule = TRUE)
-
-profiles[["EBNA3C"]] <- mclapply(file.path(profile_dir,files3),
-  load_clean_profile,window_ext, mc.cores = mc,mc.preschedule = TRUE)
-
-profiles[["EBNA2"]] <- mclapply(file.path(profile_dir,files4),
-  load_clean_profile,window_ext, mc.cores = mc,mc.preschedule = TRUE)
-
-profiles[["RBPJ"]] <- mclapply(file.path(profile_dir,files5),
-  load_clean_profile,window_ext, mc.cores = mc,mc.preschedule = TRUE)
-
-
-add_names <- function(set,data,files,mc = 24)
-{
-    out <- mcmapply(function(x,y){
-      v <- strsplit(y,"_")[[1]]
-      aux <- copy(x)
-      aux[,set := v[1]]
-      aux[,histone := v[2]]
-      aux[,rep := as.numeric(gsub("rep","",v[3]))]
-      return(aux)
-    },data[[set]],grepV(set,files),SIMPLIFY = FALSE,mc.cores = mc)
-  
-  return(out)
-}
-
-test <- list()
-test[["RBPJ"]] <- mclapply(profiles[["RBPJ"]],
-  function(x){    
-    out <- x[,mean(tagCounts,trim = .05,na.rm = TRUE),by = coord]
-    return(out)},mc.cores = mc)             
-test <- add_names("RBPJ",test,files)
-test <- do.call(rbind,test)
-
-
-pdf(file = "RBPJ.pdf")
-ggplot(test[grep("H3",histone)],
-       aes(coord,V1,colour = histone,linetype = as.factor(rep)))+
-  geom_line(linesize = 1)+scale_color_brewer(palette = "Set1")+
-  theme(panel.background = element_rect(fill = "black"),
-        panel.grid.major = element_blank(),panel.grid.minor = element_blank())+ylim(0,2)
-dev.off()
-
-
-M <- 1.5e3
+M <- 200
 lb <- -M
 ub <- M
+
 stats <- lapply(profiles,function(x,lb,ub){
   out <- mclapply(x,function(y,lb,ub){
-    y[between(coord,lb,ub),max(tagCounts)/min(tagCounts),by = match]
+    y[between(coord,lb,ub),max(tagCounts),by  = match]
   },lb,ub,mc.cores = mc)
   return(out)},lb,ub)
 
+stats <- lapply(profiles,function(x,lb,ub){
+  out <- mclapply(x,function(y,lb,ub){
+    y[between(coord,lb,ub),trapz(coord,tagCounts),by  = match]
+  },lb,ub,mc.cores = mc)
+  return(out)},lb,ub)
 
-stats <- lapply(stats,function(x)do.call(rbind,x))
+stats <- lapply(sets,add_names,stats)
+names(stats) <- sets
+
 stats <- do.call(rbind,stats)
 
-ggplot(stats[,mean(signal),by = .(match,set,histone)] ,
-       aes(histone , V1))+geom_boxplot()+
-  facet_grid( set ~ .)+scale_y_log10()+ylab("signal")+xlab("histone")+
+mat <- peak_list[["EBNA3A"]][Dnase == 1][,(match)]
+
+ggplot(stats[match %in% mat & rep <= 2],aes(histone  , V1,color = as.factor(rep)))+geom_boxplot()+
+  scale_y_log10()+facet_grid( set ~ .)+scale_color_brewer(palette = "Set1")+
   theme(axis.text.x = element_text(angle = 90))
 dev.off()
+
+
+
+
+setkey(stats,set)
+test <- copy(stats["EBNA3A"][match %in% mat & rep == 1])
+test <- dcast.data.table(data =test, match ~ histone,value.var = "V1") 
+
+heatmap(as.matrix(test[,-1,with = FALSE]),col = brewer.pal(10,"RdYlGn"),labRow = "")
+dev.off()
+
+
+
+
+
+## test <- list()
+## suppressWarnings(
+## test[[set]] <- mclapply(profiles[["EBNA3B"]],
+##   function(x){    
+##     out <- x[,mean(tagCounts,trim = .05,na.rm = TRUE),by = coord]
+##     return(out)},mc.cores = mc)
+##                  )
+## test <- add_names("EBNA3B",test,files)
+## test <- do.call(rbind,test)
+
+
+## pdf(ile = "EBNA3B_old.pdf")
+## ggplot(test[grep("H3",histone)],
+##        aes(coord,V1,colour = histone,linetype = as.factor(rep)))+
+##   geom_line(linesize = 1)+scale_color_brewer(palette = "Set1")+
+##   theme(panel.background = element_rect(fill = "black"),
+##         panel.grid.major = element_blank(),panel.grid.minor = element_blank())+ylim(0,2)
+## dev.off()
+
+
